0624a43a2b8d0647ca47e32e653483a389d96bc0
philoshan <philos0406@gmail.com>
2026-01-20 15:26:16 +0900

    error fixed, p3 worksapce is p3_cpp

---
+++ ros2_ws/build/simulator_launch/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hy/Mobility_Challenge/ros2_ws/src/simulator_launch
+# Build directory: /home/hy/Mobility_Challenge/ros2_ws/build/simulator_launch
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- ros2_ws/install/setup.ps1
+++ ros2_ws/install/setup.ps1
@@ -23,6 +23,8 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/foxy\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/hy/Mobility_Challenge/sdk/KAIST_Mobility_Challenge_SDK/examples/Driver_ROS2/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/hy/Mobility_Challenge/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
